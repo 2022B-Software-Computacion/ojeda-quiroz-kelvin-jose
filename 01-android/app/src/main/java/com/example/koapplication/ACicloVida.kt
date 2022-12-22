79932c914bc7be12275e4d33da583252c1d5b8a6
kevin ojeda <kelvin.ojeda@epn.edu.ec>
2022-12-22 16:10:59 -0500

    Lifecyle of an Activity finished and deployment to firebase to check in phone, using key and value to save data when we use portrait or landscape mode

--- 01-android/app/src/main/java/com/example/koapplication/ACicloVida.kt
+++ 01-android/app/src/main/java/com/example/koapplication/ACicloVida.kt
@@ -31,47 +31,68 @@ class ACicloVida : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-        mostrarSnackBar("onCreate")
+        mostrarSnackBar("onCreate-")
     }
 
     override fun onStart() {
         super.onStart()
-        mostrarSnackBar("onStart")
+        mostrarSnackBar("onStart-")
     }
 
     override fun onRestart() {
         super.onRestart()
-        mostrarSnackBar("onRestart")
+        mostrarSnackBar("onRestart-")
     }
 
     override fun onResume() {
         super.onResume()
-        mostrarSnackBar("onResume")
+        mostrarSnackBar("onResume-")
     }
 
     override fun onPause() {
         super.onPause()
-        mostrarSnackBar("onPause")
+        mostrarSnackBar("onPause-")
     }
 
     override fun onStop() {
         super.onStop()
-        mostrarSnackBar("onStop")
+        mostrarSnackBar("onStop-")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        mostrarSnackBar("onDestroy")
+        mostrarSnackBar("onDestroy-")
     }
 
-    fun mostrarSnackBar(texto:String){
+    fun mostrarSnackBar(texto: String) {
         textoGlobal += texto
-        Snackbar.make(findViewById(R.id.cl_ciclo_vida), textoGlobal, Snackbar.LENGTH_LONG) .setAction("Action", null).show()
+        Snackbar.make(findViewById(R.id.cl_ciclo_vida), textoGlobal, Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show()
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_aciclo_vida)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.run {
+            //Solamente podemos guardar variables primitivas
+            putString("textoGuardado", textoGlobal)
+            //putString("numeroGuardado", numero)
+
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val textoRecuperado: String? = savedInstanceState.getString("textoGuardado")
+        // val textoRecuperado:Int? = savedInstanceState.getInt("numeroGuardado")
+        if (textoRecuperado != null) {
+            mostrarSnackBar(textoRecuperado)
+            textoGlobal = textoRecuperado
+        }
     }
 }
